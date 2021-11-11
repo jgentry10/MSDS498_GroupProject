727c3ffbb470082c99193cd79bba6fce6010187c
ttocsnamfuak <30938365+ttocsnamfuak@users.noreply.github.com>
2021-11-10 20:29:08 -0600

    python flask app that calls endpoint

---
+++ test.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/predict')
+def predict():
+    import requests
+    
+    url = "https://6l6l7n7vtb.execute-api.us-east-2.amazonaws.com/test/predict"
+
+    payload="{\"data\":\"0,-0.34147611300851444,0.18202662446267728,310000.0,2.0,2.0,1.0,44.0,-1.0,-1.0,-2.0,-2.0,-2.0,-2.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\"}"
+    headers = {'Content-Type': 'application/json'}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    name = str(response.text)
+    #return render_template("predict.html",name=name)
+    return name
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port='8080')
+
