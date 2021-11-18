b477977c95298e5f4900c187f0593df49307fdc6
William <cwpan@users.noreply.github.com>
2021-11-18 00:40:09 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import numpy as np
 from flask import Flask, jsonify, request, render_template
-from array import *
+import json
 
 app = Flask(__name__)
 
@@ -15,8 +15,11 @@ def predict():
     '''
     For rendering results on HTML page
     '''
-    fl_features = [str(x) for x in request.form.values()]
-    payload="{\"data\":ftldata}"
+    fm_features = [str(x) for x in request.form.values()]
+    strdata=','.join(fm_features)
+    dictdata={"data":strdata}
+    jsondata=json.dumps(dictdata)
+    payload=json.loads(jsondata)
     url = "https://6l6l7n7vtb.execute-api.us-east-2.amazonaws.com/test/predict"
 
     #payload="{\"data\":\"0,-0.34147611300851444,0.18202662446267728,310000.0,2.0,2.0,1.0,44.0,-1.0,-1.0,-2.0,-2.0,-2.0,-2.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\"}"
@@ -24,13 +27,8 @@ def predict():
 
     response = requests.request("POST", url, headers=headers, data=payload)
     name = str(response.text)
-    #return render_template('index.html', prediction_text=name)
-    return render_template('index.html', prediction_text=fl_features)
+    return render_template('index.html', prediction_text=name)
 	
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port='8080')
-
-
-
-
